95a4461d24b37f30c4214b681007f91a5925af84
Vikrant Chauhan <vikrantchauhan1047@gmail.com>
2025-03-17 17:24:13 +0530

    room set up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,26 @@
 import { useEffect,useState } from "react";
 import { supabase } from "../supabase";
 import { Navigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+import { useNavigate } from "react-router-dom";
 
 
 
 
 const Home=()=>{
+
+    const navigate = useNavigate();
+
+
     const [user,setUser]= useState(null);
     const[loading,setLoading]= useState(true);
+
+    const createRoom= ()=>{
+        const roomId =  uuidv4(); // This will generate a unique room id 
+        navigate(`/room/${roomId}`); // Redirecting to the room page
+    }
+
+
     useEffect(()=>{
         const getUser= async()=>{
             const {data:{user}}= await supabase.auth.getUser();
@@ -28,8 +41,15 @@ const Home=()=>{
         <div>
             {user? 
                 <div>
-                    <p>Welcome to synctube {user.email} darling</p>
+                    <p>Welcome to synctube {user.email} </p>
                     <button onClick={signOut}>Logout</button>
+                    <h1 className="text-2xl font-bold mb-4">Create a YouTube Watch Party</h1>
+                    <button 
+                        onClick={createRoom} 
+                        className="bg-blue-500 text-white px-4 py-2 rounded-lg"
+                    >
+                        Create Room
+                    </button>
                 </div>
              : 
              <Navigate to={"/login"} />}
---
+++ src/pages/Room.jsx
@@ -0,0 +1,6 @@
+import { useParams } from "react-router-dom";
+
+const Room = ()=>{
+    const {roomId} = useParams(); // getting the room id from the URL
+    
+}
