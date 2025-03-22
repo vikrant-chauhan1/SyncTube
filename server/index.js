708e33175ec6320aee66b5475dcefc073a952a06
Vikrant Chauhan <vikrantchauhan1047@gmail.com>
2025-03-23 02:40:01 +0530

    yt player added

---
+++ server/index.js
@@ -0,0 +1,5 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,19 +1,60 @@
 import { useParams } from "react-router-dom";
+import YouTube from "react-youtube";
+import { useState } from "react";
 
 const Room = ()=>{
-    const {roomId} = useParams(); // getting the room id from the URL
+  const {roomId} = useParams(); // getting the room id from the URL
+  const [videoId,setVideoId]= useState("") // will use this to store the video ID
+  const [videoUrl,setVideoUrl] = useState("");// storing the video url
 
+  const extractVideoId = (url)=>{
+    const regex = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/;
+    const match = url.match(regex);
+    return match ? match[1] : "";
+  }
+
+  // handling the form submittion 
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    const id= extractVideoId(videoUrl)
+    setVideoId(id);
+  }
+  const opts = {
+    height:"399",
+    width:"640",
+    playerVars:{
+      autoplay:1,
+    },
+  };
     return (
-        <div >
-          <h1 >Room ID: {roomId}</h1>
-          <p>Share this link to invite friends!</p>
-          <input 
-            type="text" 
-            value={window.location.href} 
-            readOnly 
+      <div >
+        <h1 >Room ID: {roomId}</h1>
+        <p>Share this link to invite friends!</p>
+        <input 
+          type="text" 
+          value={window.location.href} 
+          readOnly 
             
+        />
+
+        <form action="submit" onSubmit={handleSubmit}>
+          
+          <input
+            type="text"
+            placeholder="Enter youTube link "
+            value={videoUrl}
+            onChange={(e)=>setVideoUrl(e.target.value)}
+        
           />
-        </div>
+          <button type="submit">
+            Play Video
+
+          </button>
+          
+        </form>
+
+        {videoId && <YouTube videoId={videoId} opts={opts} />}
+      </div>
     );
     
 }
