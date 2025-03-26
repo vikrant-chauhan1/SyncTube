0e6390ffbe4a523282796cad43b8e58f6b8e60b8
Vikrant Chauhan <vikrantchauhan1047@gmail.com>
2025-03-26 19:34:32 +0530

    socket io implementation

--- server/index.js
+++ server/index.js
@@ -1,5 +1,34 @@
 import express from "express";
-import http from "http";
-import { Server } from "socket.io";
-import cors from "cors";
+
+import { io } from "socket.io";
+
+
+const app = express();
+const socket = io("http://localhost:4000", { transports: ["websocket"] });
+
+socket.on("connection",(socket)=>{
+    console.log(`User connected : ${socket.id}`);
+
+    socket.on("set_video", ({ roomId, videoId }) => {
+        console.log(`received video id ${videoId} and room id ${roomId}`)
+        socket.to(roomId).emit("load_video", videoId); // Broadcast video to everyone
+    });
+
+    socket.on("join_room",(roomId)=>{
+       socket.join(roomId);
+       console.log(`User joined the room ${roomId}`) 
+    });
+
+    socket.on("video_control",({roomId,action,time})=>{
+        socket.to(roomId).emit("sync_video",{action,time});
+    });
+
+    socket.on("disconnect",()=>{
+        console.log(`User disconnected ${socket.id}`)
+    });
+});
+
+
+
+
 
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,12 +1,36 @@
 import { useParams } from "react-router-dom";
 import YouTube from "react-youtube";
-import { useState } from "react";
+import { useState,useEffect,useRef } from "react";
+import { io } from "socket.io-client";
+
+const socket= io("http://localhost:4000"); //connect to backend
 
 const Room = ()=>{
   const {roomId} = useParams(); // getting the room id from the URL
   const [videoId,setVideoId]= useState("") // will use this to store the video ID
   const [videoUrl,setVideoUrl] = useState("");// storing the video url
+  const playerRef= useRef(null);
 
+  useEffect(()=>{
+    socket.emit("join_room",roomId);
+
+    socket.on("load_video", (videoId) => {
+      console.log("Received new video id : ",videoId)
+      setVideoId(videoId); // When a new user joins, load the video
+    });
+    socket.on("sync_video",({action,time})=>{
+      const player = playerRef.current;
+      if(!player) return
+
+      if(action === "play") player.playVideo();
+      if(action === "pause") player.pauseVideo();
+      if(action === "seek") player.seekTo(time,true);
+    });
+
+    return()=>{
+      socket.off("sync_video"); // i guess here we are turning off the sync video connection with the server
+    }
+  },[roomId])
   const extractVideoId = (url)=>{
     const regex = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/;
     const match = url.match(regex);
@@ -17,8 +41,24 @@ const Room = ()=>{
   const handleSubmit = (e)=>{
     e.preventDefault();
     const id= extractVideoId(videoUrl)
-    setVideoId(id);
+    if (id) {
+      console.log("submitting the video id ")
+      setVideoId(id); // Schedule state update
+      socket.emit("set_video", { roomId, videoId: id }); // Emit latest video immediately
+    }
   }
+
+  const handleStateChange = (event)=>{
+    const player = playerRef.current;
+    const time = player.getCurrentTime();
+
+    if(event.data === 1) socket.emit("video_control",{roomId,action:"play",time});
+    if(event.data === 2) socket.emit("video_control",{roomId,action:"pause",time});
+
+  }
+
+ 
+
   const opts = {
     height:"399",
     width:"640",
@@ -53,7 +93,16 @@ const Room = ()=>{
           
         </form>
 
-        {videoId && <YouTube videoId={videoId} opts={opts} />}
+        {videoId ? (
+          <YouTube 
+            videoId={videoId} 
+            opts={opts} 
+            onStateChange={handleStateChange} 
+            onReady={(event) => (playerRef.current = event.target)} 
+          />
+          ) : (
+          <p>Waiting for video...</p>
+        )}
       </div>
     );
     
